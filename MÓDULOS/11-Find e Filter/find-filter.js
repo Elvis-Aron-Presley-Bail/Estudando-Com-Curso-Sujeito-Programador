6f1be2648f3910cd161477749aa34b93c550f4a5
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2026-03-11 12:06:00 -0300

    Aprendendo Find e Filter

--- MÓDULOS/11-Find e Filter/find-filter.js
+++ MÓDULOS/11-Find e Filter/find-filter.js
@@ -1,11 +1,25 @@
-//FIND
+//FIND = Devolve o primeiro que encontra
 
 let lista = [5, 2, 98, 'João', 78, 'Pedro']
 
 let busca = lista.find((elemento) => {
-    if(elemento === 'João') {
+    if(elemento === 'joao') {
         return elemento
+    } else {
+        return console.log('Elemento não encontrado')
     }
 })
 
 console.log(busca)
+
+
+//FILTER = Devolve tudo que encontrar
+
+let nomes = ['Joao', 'Pedro', 'Ana', 'Paulo']
+
+let encontrar = nomes.filter((nome) => {
+    return nome.length <= 4
+})
+
+console.log(encontrar)
+
